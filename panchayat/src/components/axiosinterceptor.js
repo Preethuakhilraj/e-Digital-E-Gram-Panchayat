2b94352707797a995c21e9244b667b15bb64bd90
Preethuakhilraj <46960642+Preethuakhilraj@users.noreply.github.com>
2025-03-27 19:58:15 +0530

    Update axiosinterceptor.js

--- panchayat/src/components/axiosinterceptor.js
+++ panchayat/src/components/axiosinterceptor.js
@@ -1,17 +1,20 @@
 import axios from "axios";
-const axiosInstance=axios.create({
-//baseURL:'http://localhost:4000'
-baseURL:'https://e-digital-e-gram-panchayat-backend.vercel.app'
-})
-axiosInstance.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token'); // Adjust token retrieval logic as necessary
+
+const axiosInstance = axios.create({
+  baseURL: 'https://e-digital-e-gram-panchayat-server.vercel.app' 
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token'); 
     if (token) {
       config.headers['Authorization'] = `Bearer ${token}`;
     }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
-return config;}
-,(error)=>{
-    return Promise.reject(error)
-}
-)
-export default axiosInstance
+export default axiosInstance;
